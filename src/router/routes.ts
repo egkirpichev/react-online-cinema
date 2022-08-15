457cdcfeee6b5062d0a97d3740d37f6ffe91e8c6
unknown <egkirpichev@gmail.com>
2022-08-15 19:32:23 +0300

    feat(routes): add basic routes enum

---
+++ src/router/routes.ts
@@ -0,0 +1,9 @@
+export enum Routes {
+  HOME = "/",
+  TRENDS = "/trends",
+  FVOURITES = "/favourites",
+  SETTINGS = "/setting",
+  SIGN_IN = "/signin",
+  SIGN_UP = "/signup",
+  RESET_PASSWORD = "/reset_password",
+}

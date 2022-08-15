6e52deefaac378d572c66549c655ee17eb21d8c5
unknown <egkirpichev@gmail.com>
2022-08-15 20:51:37 +0300

    feat(design): plug in fonts, create Global Styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import { GlobalStyles } from "./ui";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyles />
     <BrowserRouter>
       <App />
     </BrowserRouter>

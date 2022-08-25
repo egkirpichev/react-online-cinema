15a050a3d6a7cafa27449c30bc622f7c82efb862
unknown <egkirpichev@gmail.com>
2022-08-25 23:34:27 +0300

    feat(components): add MovieCard component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { OMDbApi } from "../../services/OMDbApi";
+import { IMovieFull } from "../../types";
+import { Color } from "../../ui";
+import { Spinner } from "../MovieList/styles";
+import { ErrorMessage } from "../MovieListItem/styles";
+import { MoviePoster } from "../MoviePoster";
 import { StyledMovieCard } from "./styles";
 
 export const MovieCard = () => {
-  return <StyledMovieCard>MovieCard</StyledMovieCard>;
+  const [movie, setMovie] = useState<IMovieFull>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const { imdbID } = useParams();
+
+  useEffect(() => {
+    if (imdbID)
+      OMDbApi.getMovieById(imdbID)
+        .then((movie) => {
+          setMovie(movie);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          setIsLoading(false);
+          setErrorMessage(error.message);
+        });
+  }, [imdbID]);
+
+  if (isLoading) {
+    return <Spinner color={Color.PrimaryDark} />;
+  }
+
+  if (errorMessage) {
+    return <ErrorMessage>Something went wrong 🎞</ErrorMessage>;
+  }
+
+  if (movie) {
+    return (
+      <StyledMovieCard>
+        <MoviePoster poster={movie.Poster}></MoviePoster>
+      </StyledMovieCard>
+    );
+  } else return <ErrorMessage>Something went wrong 🎞</ErrorMessage>;
 };

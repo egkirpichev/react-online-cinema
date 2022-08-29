1d995bf350a50d851e446e28ab39a80299d1f83b
Egor G Kirpichev <Egor.Kirpichev@corp.b1.by>
2022-08-29 17:29:21 +0300

    refactor(components): make Input component's title optional

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,10 +1,14 @@
 import { Subtitle3 } from "../../ui/typography";
 import { StyledInput, Wrapper } from "./styles";
 
-export const Input = () => {
+interface IProps {
+  title?: string;
+}
+
+export const Input = ({ title }: IProps) => {
   return (
     <Wrapper>
-      <Subtitle3>Title</Subtitle3>
+      {title && <Subtitle3>{title}</Subtitle3>}
       <StyledInput placeholder="Placeholder" type="text" />
     </Wrapper>
   );

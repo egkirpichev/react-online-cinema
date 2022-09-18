f31f8ef8474ce20c99d7bf9e4f2ee220d0de9a01
unknown <egkirpichev@gmail.com>
2022-09-18 15:19:39 +0300

    refactor(router): swith back to browser router

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { App } from "./App";
 import { persistor, store } from "./store/store";
@@ -10,13 +10,13 @@ import { GlobalStyles } from "./ui";
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
+          <GlobalStyles />
           <App />
         </PersistGate>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 );
